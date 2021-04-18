056fc7fccfd9be67ab1cedad0b00213627ef3ed1
Javi <javi.cb@gmail.com>
2021-04-18 20:00:51 +0200

    graficas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,8 @@ import { SharedModule } from '../shared/shared.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ComponentsModule } from '../components/components.module';
 
+import { ChartsModule } from 'ng2-charts';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -22,7 +24,8 @@ import { MainComponent } from './main/main.component';
     CommonModule,
     PagesRoutingModule,
     SharedModule,
-    ComponentsModule
+    ComponentsModule,
+    ChartsModule
   ]
 })
 export class PagesModule { }

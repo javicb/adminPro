85d25350b3fc9b571d69132b970bea5042a70caf
Javier Cruz <javi.cb@gmail.com>
2021-04-24 01:00:00 +0200

    crear observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -8,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    const observable = new Observable(observer => {
+      let i = -1;
+
+      const intervalo = setInterval(() => {
+        i++;
+        observer.next(i);
+
+        if (i === 4) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+
+        if (i === 2) {
+          observer.error('i tiene valor 2');
+        }
+      }, 1000)
+    });
+
+    observable.subscribe(
+      valor => console.log('Subs', valor),
+      err => console.warn('Error', err),
+      () => console.info('observable terminado')
+    );
+  }
 
   ngOnInit(): void {
   }
